1d6772453ae930de29f669b0aa07be4c193199bc
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-05-25 22:44:33 +0600

    print using for loop

--- PrintTable.java
+++ PrintTable.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 class PrintTable {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter a number that you want to print table ");
+
+
+        System.out.println("Enter a number that you want to print table    ");
         int no = s.nextInt();
         for (int i = 1; i <= 10; i++) {
             System.out.println(no + "*" + i + "=" + no * i);
