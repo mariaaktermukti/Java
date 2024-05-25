9c1cee709b154cd389f6388e6857c602feeb78d5
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-05-25 12:40:52 +0600

    Swap without 3rd variable

---
+++ Swap_without_3rd_variable.java
@@ -0,0 +1,10 @@
+public class Swap_without_3rd_variable {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+      System.out.println("a :"+a);
+      System.out.println("b :"+b);
+    }
+}
