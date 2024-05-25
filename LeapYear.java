95f0f59550dba660e54d7f81de05933e569f3978
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-05-25 12:19:38 +0600

    LeapYear

---
+++ LeapYear.java
@@ -0,0 +1,12 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year=2016;
+        if((year%400==0) || (year%4==0 && year%100!=0))
+        {
+            System.out.println("Leap year");
+        }
+        else{
+            System.out.println("Not a leap year");
+        }
+    }
+}
