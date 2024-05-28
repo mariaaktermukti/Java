914f71fa71c8a55cd3a26fa5a3129113ee8340ed
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-05-28 23:03:03 +0600

    Single_inheritance.java

---
+++ Single_inheritane.java
@@ -0,0 +1,22 @@
+class A {
+    void showA() {
+        System.out.println("a class method");
+    }
+
+}
+
+class B extends A {
+    void showB() {
+        System.out.println("b class method");
+    }
+
+    public static void main(String[] args) {
+        A ob1 = new A();
+        ob1.showA();
+
+        B ob2 = new B();
+        ob2.showA();
+        ob2.showB();
+
+    }
+}
