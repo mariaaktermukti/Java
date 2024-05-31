3ff284aac35835841c1e753a755c464ce52a7628
mariaaktermukti <mariaaktermukti069@gmail.com>
2024-05-31 08:35:17 +0600

    overloading.java

---
+++ overloading.java
@@ -0,0 +1,15 @@
+class Test {
+    void show(int a) {
+        System.out.println("1");
+    }
+
+    void show(String a) {
+        System.out.println("2");
+    }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.show("mukti");
+    }
+
+}
