5a2da0cba65a60038bc92bbce6993b39bf70e3f9
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-10-25 20:21:38 +0600

    Add files via upload

---
+++ Animal_1.java
@@ -0,0 +1,15 @@
+/*class object and method related */
+class Animal
+{
+  public void eat()
+  {
+    System.out.println("I am eating");
+  }
+
+  public static void main(String[] args)
+  {
+     System.out.println("1");
+     Animal buzo = new Animal();
+     buzo.eat();
+  }
+}
