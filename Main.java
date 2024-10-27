61d40ac68d0e4ccfd6a4373a39333ce69a64494c
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-10-27 22:09:01 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,35 +1,14 @@
-// Main.java
-class Dog extends Animal {
-
-//    void eat() {
-//        System.out.println("I am eating");
-//    }
-//
-//    void call(String name){
-//        System.out.println("Hello I am calling "+name);
-//    }
-
-
-    void bark() {
-        System.out.println("I am barking");
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
-        // Creating an instance of Dog
-        Dog dog = new Dog();
-        Dog dog2 = new Dog();
-
-        // Demonstrating IS-A relationship
-        dog.eat();   // Dog can use Animal's eat() method because Dog "IS-A" Animal
-        dog.bark();
-        // Dog's own method
-
-        System.out.println(dog.houseName);
-
-        // Creating an instance of Animal
-        Animal animal = new Animal();
-        animal.eat(); // Animal can only eat, it doesn't have bark method
+        Car myCar = new Car(); // Car IS-A Vehicle
+        myCar.start();         // Inherited from Vehicle
+        myCar.drive();         // Specific to Car
+    }
+}
+// Main.java
+class Car extends Vehicle {
+    void drive() {
+        System.out.println("Car is driving");
     }
 }
