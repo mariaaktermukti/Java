4dcb956ca36137d493c9d3a99e1da3542941bbea
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-10-29 19:47:04 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,13 +2,25 @@ class B extends A{
     void showB(){
         System.out.println("b class method");
     }
+}
+class C extends B{
+    void showC(){
+        System.out.println("c clas method");
+    }
 
     public static void main(String[] args) {
-        A ob1= new A();
+        A ob1=  new A();
         ob1.showA();
+        System.out.println("--------------------");
 
         B ob2= new B();
         ob2.showA();
         ob2.showB();
+        System.out.println("--------------------");
+
+        C ob3=new C();
+        ob3.showA();
+        ob3.showB();
+        ob3.showC();
     }
 }
