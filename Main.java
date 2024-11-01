0c271b58329ac04c013f30d2b370d4053a1ba42b
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-11-01 22:10:23 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
-// This file contains the main method and uses the Show class
 public class Main {
-    public static void main(String[] args) {
-        // Creating an object of the Show class
-        Show show = new Show();
+    public static void main(String[] args){
+        Make m= new Make();
+        m.Make();
 
-        // Calling different versions of the 'display' method
-        show.display(5); // Calls the method that takes one integer
-        show.display("Hello!"); // Calls the method that takes a string
-        show.display(10, 20); // Calls the method that takes two integers
+        Xyz x=new Xyz();
+        x.Make();
+
     }
 }
---
+++ Make.java
@@ -0,0 +1,10 @@
+class Make{
+    void Make(){
+        System.out.println("1");
+    }
+}
+class Xyz extends  Make{
+    void Make(){
+        System.out.println("2");
+    }
+}
