71fc4b98ad07821d0041aca732e31be16d38decc
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-11-03 20:47:55 +0600

    Add files via upload

---
+++ FancyIO.java
@@ -0,0 +1,7 @@
+public class FancyIO
+{
+    public static void main(String[] args) {
+        char ch= 0x0986; //unicode
+        System.out.println(ch);
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String args[])
+    {
+        System.out.print("Hello World!");
+        System.out.println(" Its Java!");
+        int x=10;
+        int y=5;
+        int z=x+y;
+        System.out.println(z);
+      String str = "Bangladesh";
+        System.out.println(str);
+
+        System.out.printf("This is a string %s\n", str);
+    }
+}
