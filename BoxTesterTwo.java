4efb01257256a3623810a259b31912ae95052d5a
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-11-10 08:44:12 +0600

    Add files via upload

---
+++ BoxTesterTwo.java
@@ -0,0 +1,15 @@
+import PackageOne.Box;
+package PackaeTwo;
+
+public class BoxTesterTwo
+{
+    public static void main(String[] args) {
+        Box b= new Box();
+
+    }
+
+    protected void proFunct()
+    {
+        System.out.println("In the protected function");
+    }
+}
