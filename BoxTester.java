a3ffcb410c9e753129483291795dd9d9859d832c
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-11-10 08:43:04 +0600

    Add files via upload

---
+++ BoxTester.java
@@ -0,0 +1,11 @@
+import PackageOne.Box;
+
+public class BoxTester
+{
+    public static void main(String[] args) {
+        Box b=new Box();
+        System.out.println(b.y);
+        b.pubFunct();
+        System.out.println(b.z);
+    }
+}
---
+++ RectangleTesterThree.java
@@ -0,0 +1,8 @@
+public class RectangleTesterThree
+{
+    public static void main(String[] args) {
+        Rectangle rec1= new Rectangle();
+        System.out.println(rec1.getHeight());
+        System.out.println(rec1.getWidth());
+    }
+}
