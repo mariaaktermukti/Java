ef565779adca87a696f42cd3c60b334be6466a70
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2024-11-11 21:50:25 +0600

    Add files via upload

---
+++ Hi.java
@@ -0,0 +1,11 @@
+public class Hi
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Before SayHi");
+          SayHi();
+    }
+    public static void SayHi(){
+        System.out.println("Hi!!");
+    }
+}
